46f6c3f6e108dc88841e2fdc7e19d23aaab1cb8c
suil0304 <eagle030410@gmail.com>
2026-03-19 18:40:53 +0900

    test

--- js/map.js
+++ js/map.js
@@ -1,10 +1,16 @@
-var mapContainer = document.getElementById('map'), 
+
+window.onload = function() {
+    var mapContainer = document.getElementById('map'), 
     mapOption = { 
         center: new kakao.maps.LatLng(37.566826, 126.9786567),
         level: 3 
     };
 
-var map = new kakao.maps.Map(mapContainer, mapOption);
+    var map = new kakao.maps.Map(mapContainer, mapOption);
+
+    
+    console.log("지도 로드 성공!");
+};
 
 function calculateRoute() {
     // 1. 입력된 주소를 좌표로 변환 (Geocoding)
