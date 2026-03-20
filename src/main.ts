143e354dc8d7312fa4aaa01d392769cb65ff0fa1
suil0304 <eagle030410@gmail.com>
2026-03-20 11:40:04 +0900

    index.html 및 main.ts 파일 수정: 카카오 맵 API 스크립트 위치 변경 및 map.ts 파일 삭제

---
+++ src/main.ts
@@ -0,0 +1,38 @@
+/// <reference path="../node_modules/kakao.maps.d.ts/@types/index.d.ts" />
+import type {KakaoRouteResponse} from "../@types/types"
+
+var mapContainer:HTMLElement;
+var result:HTMLElement;
+const initMap = () => {
+    mapContainer = document.getElementById('map') as HTMLElement;
+    if(!mapContainer) return;
+
+    result = document.getElementById('result') as HTMLElement;
+
+    var mapOption = { 
+        center: new kakao.maps.LatLng(37.566826, 126.9786567),
+        level: 3 
+    };
+
+    const map = new kakao.maps.Map(mapContainer, mapOption);
+}
+
+window.onload = initMap;
+
+function calculateRoute():any {
+         
+}
+
+async function getCost():Promise<void> {
+    const origin = "127.1,37.5";
+    const destination = "127.2,37.6";
+    
+    const response = await fetch(`https://time-x-distance-money.vercel.app/api/get-route?origin=${origin}&destination=${destination}`);
+    const data = await response.json() as KakaoRouteResponse;
+
+    const taxiFare = data.routes[0].summary.fare.taxi;
+    const tollFare = data.routes[0].summary.fare.toll;
+
+
+    result.innerText = `편도 택시비: ${taxiFare}원, 통행료: ${tollFare}원`;
+}
